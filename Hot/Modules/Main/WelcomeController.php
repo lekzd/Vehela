286267dc906c08a325016f2bc371b92fd6378fe2
Ilya I. Vorozhbit <ilya@mctop.im>
2013-07-30 23:48:02 +0400

    Забыл (:

--- Hot/Modules/Main/WelcomeController.php
+++ Hot/Modules/Main/WelcomeController.php
@@ -2,8 +2,13 @@
 
     Class WelcomeController extends Controller{
 
+        public function Init(){
+
+        }
+
         public function Hello(){
-            $this->Render($this->View);
+            $this->MakeStampInView('test','hello');
+            $this->MakeStampInLayout('Title','hello');
         }
 
     }
--- Hot/Prototypes/Controller.php
+++ Hot/Prototypes/Controller.php
@@ -7,15 +7,27 @@
         public function __construct($View){
             $this->View = $View;
             $this->Init();
-            //$this->Render($View);
+        }
+
+        public function __destruct(){
+            $this->Render();
         }
 
         public function Init(){
-
         }
 
-        public function Render($View){
-            print($View->Views);
+        public function Render(){
+            $this->View->View = str_replace('{$Content}','test',$this->View->View);
+            $this->View->Layout = str_replace('{$Content}',$this->View->View,$this->View->Layout);
+            print($this->View->Layout);
+        }
+
+        public function MakeStampInView($VariableName, $Value){
+            $this->View->View = str_replace('{$'.$VariableName.'}',$Value,$this->View->View);
+        }
+
+        public function MakeStampInLayout($VariableName, $Value){
+            $this->View->Layout = str_replace('{$'.$VariableName.'}',$Value,$this->View->Layout);
         }
 
     }
