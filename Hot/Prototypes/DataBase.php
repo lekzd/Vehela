605d4d086c26b035d8ddfdf0db9d22ce238b095a
electronrussia <bm.kot.urs@mail.ru>
2013-08-02 00:02:35 +0400

    Используем PDO

--- Hot/Prototypes/DataBase.php
+++ Hot/Prototypes/DataBase.php
@@ -3,48 +3,57 @@
 class DataBase
 {
 
-    public $host;
+    public $dsn;
     public $user;
     public $password;
-    public $db;
-    private $db_resource;
-    private $settings;
+    private $db_obj;
 
     public function __construct($settings)
     {
         foreach ($settings as $key => $parameter) {
             $this->$key = $parameter;
         }
-
         $this->Connect();
-        $this->SelectDB($this->db);
     }
 
     public function Connect()
     {
-        $this->db_resource = new mysqli($this->host, $this->user, $this->password);
-        if ($this->db_resource->connect_errno == 0) {
-            $this->db_resource->set_charset('utf8');
-        } else {
-            die($this->db_resource->connect_error);
+        try {
+            $this->db_obj = new PDO($this->dsn, $this->user, $this->password);
+            $this->exec('SET NAMES `utf8`;');
+        } catch (PDOException $e) {
+            die('Не удалось установить соединение с базой данных: ' . $e->getMessage());
         }
     }
 
-    public function SelectDB($dbname)
+    public function SelectDB($name)
     {
-        $this->db_resource->select_db($dbname);
-        if ($this->db_resource->errno != 0) {
-            die($this->db_resource->error);
-        }
+        $this->query("USE `{$name}`;");
+    }
+
+    public function getConnection()
+    {
+        return $this->db_obj;
     }
 
     public function query($query)
     {
-        $result = $this->db_resource->query($query);
-        if ($this->db_resource->errno == 0) {
+        $result = $this->db_obj->query($query);
+        if ($this->db_obj->errorCode() == 0000) {
             return $result;
         } else {
-            # die($this->db_resource->error);
+            die($this->db_obj->errorInfo()[2]);
+            return false;
+        }
+    }
+
+    public function exec($query)
+    {
+        $result = $this->db_obj->exec($query);
+        if ($this->db_obj->errorCode() == 0000) {
+            return $result;
+        } else {
+            die($this->db_obj->errorInfo()[2]);
             return false;
         }
     }
