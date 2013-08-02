fd447aa97875c876b1049191ec4a80de46e41fee
electronrussia <electronrussia93@gmail.com>
2013-08-02 16:19:30 +0400

    Small changes

--- hot/systems/Tools.php
+++ hot/systems/Tools.php
@@ -1,74 +1,56 @@
 <?php
 
-    Class Tools {
+class Tools
+{
 
-
-        public static function getIncludedFiles(){
-
-            $included_files = get_included_files();
-
-            foreach($included_files as $key => $filePath){
-
-                $included_files[$key] = array();
-
-                $fileExtension = Tools::getFileExtensionFromFilePath($filePath);
-
-                $fileName = Tools::getFileNameFromFilePath($filePath);
-
-                $included_files[$key]['name'] = $fileName;
-                $included_files[$key]['extension'] = $fileExtension;
-
-            }
-
-            return $included_files;
-
+    public static function getIncludedFiles()
+    {
+        $included_files = get_included_files();
+        $included = [];
+        foreach ($included_files as $filePath) {
+            $fileExtension = Tools::getFileExtensionFromFilePath($filePath);
+            $fileName = Tools::getFileNameFromFilePath($filePath);
+            $included[] = [
+                'name' => $fileName,
+                'extension' => $fileExtension
+            ];
         }
-
-        public static function checkForIncluded($fileName){
-
-            $includedFiles = Tools::getIncludedFiles();
-
-            foreach($includedFiles as $key => $file){
-                if($file['name'] == $fileName)
-                return 1;
-            }
-
-            return 0;
-
-        }
-
-        public static function includeFile($filePath){
-
-            require_once($filePath);
-        }
-
-        private static function getFileNameFromFilePath($filePath){
-
-            $slashPosition = strrpos($filePath,'\\');
-
-            $FileName = substr($filePath,$slashPosition+1);
-
-            $FileName = substr($FileName,0,strlen($FileName)-4);
-
-            unset($filePath);
-            unset($slashPosition);
-
-            return $FileName;
-        }
-
-        private static function getFileExtensionFromFilePath($FilePath){
-
-            $extPosition = strrpos($FilePath,'.');
-
-            $FileExtension = substr($FilePath,$extPosition+1);
-
-            unset($FileName);
-            unset($extPosition);
-
-            return $FileExtension;
-        }
-
+        unset($included_files);
+        return $included;
     }
 
+    public static function checkForIncluded($fileName)
+    {
+        $includedFiles = Tools::getIncludedFiles();
+        foreach ($includedFiles as $file) {
+            if ($file['name'] == $fileName)
+                return true;
+        }
+        return false;
+    }
+
+    public static function includeFile($filePath)
+    {
+        require_once($filePath);
+    }
+
+    private static function getFileNameFromFilePath($filePath)
+    {
+        $slashPosition = strrpos($filePath, '\\');
+        $FileName = substr($filePath, $slashPosition + 1);
+        $FileName = substr($FileName, 0, strlen($FileName) - 4);
+        unset($filePath, $slashPosition);
+        return $FileName;
+    }
+
+    private static function getFileExtensionFromFilePath($FilePath)
+    {
+        $extPosition = strrpos($FilePath, '.');
+        $FileExtension = substr($FilePath, $extPosition + 1);
+        unset($extPosition);
+        return $FileExtension;
+    }
+
+}
 
 ?>
